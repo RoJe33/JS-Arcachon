b923e4a72b57e84919b1dbd461e282b4821c8f2e
RoJe <chambres.jeremy@gmail.com>
2024-02-22 16:24:08 +0100

    second pane en cours

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -4,6 +4,21 @@ async function testWork(){ //fonction de récup de l'api
 }
 let fetchdJson = testWork();
 let listProjects = document.getElementById("listProjects");
+let login = document.getElementById("login");
+let myButtons = document.getElementById("myButtons");
+let myProjects = document.getElementById("myProjects");
+let panelModify = document.getElementById("panelModify");
+let panelApi = document.getElementById("panel-data-api");
+let childPanelApi = document.getElementById("child-panel-api");
+let X = document.getElementById("X");
+let panelProjects = document.getElementById("panelProjects");
+let addProject = document.getElementById("addProject");
+let addProjectPanel = document.getElementById("addProjectPanel");
+let childAddPanel = document.getElementById("child-add-panel");
+let closeAdd = document.getElementById("closeAdd");
+let returnPanel = document.getElementById("returnPanel");
+
+
 
 function changeButtons(but1){ // fonction de changement de couleur des boutons
     all.classList.contains('text-white') ? (all.classList.remove('bg-teal-700', 'text-white'), all.classList.add('text-teal-700')) : "";  
@@ -63,3 +78,106 @@ hotel.addEventListener('click',function(event){
 });
 
 displayData();
+
+localStorage.getItem("token") ? login.textContent = "logout" : '';
+
+if(localStorage.getItem("token")){
+    myButtons.classList.add("hidden")
+    panelModify.classList.remove("hidden")
+}
+
+function showPanelApi(){
+    panelApi.classList.remove("hidden");
+    setTimeout(()=> {
+        panelApi.classList.add("opacity-100");
+        panelApi.classList.remove("opacity-0");
+    })
+}
+function hidePanelApi(){
+    panelApi.classList.remove("opacity-100");
+    panelApi.classList.add("opacity-0");
+    setTimeout(()=> {
+        panelApi.classList.add("hidden");
+    }, 500)
+}
+function displayDataPanel(){ 
+    panelProjects.textContent = '';
+    fetchdJson.then(function(result) {
+        result.forEach(element => {
+            let div = panelProjects.appendChild(document.createElement("div"));
+            div.classList.add('relative')
+            let thrash = div.appendChild(document.createElement('div'));
+            thrash.classList.add('fixed','ml-8','mt-1', 'z-10', 'bg-black','h-5', 'w-5', 'flex', 'justify-center', 'items-center', 'cursor-pointer');
+            thrash.innerHTML = "<i class=\"fa-solid fa-trash text-white\"></i>";
+            let image = div.appendChild(document.createElement('img'));
+            image.src = element.imageUrl;
+            image.classList.add('object-contain', 'max-h-20');
+            let p = div.appendChild(document.createElement("p"));
+            p.classList.add('text-sm', 'py-2');
+            p.textContent = "éditer";
+        });
+    })
+}
+
+panelModify.addEventListener('click', function(event){
+    showPanelApi();
+    displayDataPanel();
+})
+
+childPanelApi.addEventListener('click', function(event){
+    event.stopPropagation   ;
+})
+
+panelApi.addEventListener('click',function(event){
+    if(event.target === panelApi){
+        hidePanelApi();
+    }
+})
+X.addEventListener('click',function(event){
+        hidePanelApi();
+})
+
+function showAddPanel(){
+    addProjectPanel.classList.remove("hidden");
+    addProjectPanel.classList.add("opacity-100");
+    addProjectPanel.classList.remove("opacity-0");
+    panelApi.classList.remove("opacity-100");
+    panelApi.classList.add("opacity-0");
+    setTimeout(()=> {
+        panelApi.classList.add("hidden");
+    }, 500)
+}
+function hideAddPanel(){
+    addProjectPanel.classList.remove("opacity-100");
+    addProjectPanel.classList.add("opacity-0");
+    setTimeout(()=> {
+        addProjectPanel.classList.add("hidden");
+    }, 500)
+}
+
+addProject.addEventListener('click', function(event){
+    showAddPanel();
+})
+closeAdd.addEventListener('click', function(event){
+    hideAddPanel();
+})
+childAddPanel.addEventListener('click', function(event){
+    event.stopPropagation   ;
+})
+
+addProjectPanel.addEventListener('click',function(event){
+    if(event.target === addProjectPanel){
+        hideAddPanel();
+    }
+})
+
+returnPanel.addEventListener('click', function(event){
+    panelApi.classList.remove("hidden");
+    panelApi.classList.add("opacity-100");
+    panelApi.classList.remove("opacity-0");
+    addProjectPanel.classList.remove("opacity-100");
+    addProjectPanel.classList.add("opacity-0");
+    setTimeout(()=> {
+        addProjectPanel.classList.add("hidden");
+    }, 500)
+})
